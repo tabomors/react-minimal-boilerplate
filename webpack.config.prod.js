f93b6310831b6ae85f1862f52ef7d7ca755d1734
Stas Morozevich <stas.morozevich@gmail.com>
2019-05-29 14:50:23 +0300

    Add bundle analyzer

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,15 +1,17 @@
 const merge = require('webpack-merge');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 const baseConfig = require('./webpack.config.base.js');
 
 module.exports = merge(baseConfig, {
-  mode: "production",
+  mode: 'production',
   output: {
     publicPath: './'
   },
   optimization: {
     minimizer: [new OptimizeCSSAssetsPlugin({}), new UglifyJsPlugin({ test: /\.js(\?.*)?$/i })]
-  }
+  },
+  plugins: [new BundleAnalyzerPlugin({ analyzerMode: 'static', openAnalyzer: false })]
 });
