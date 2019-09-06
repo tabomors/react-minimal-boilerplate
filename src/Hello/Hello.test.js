d0811d47a4df994fcae2ca33297bb94519b492ca
Stas Morozevich <stas.morozevich@gmail.com>
2019-09-07 00:18:51 +0300

    Use @testing-library/react package

--- src/Hello/Hello.test.js
+++ src/Hello/Hello.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'react-testing-library';
+import { render } from '@testing-library/react';
 import Hello from './Hello';
 import { FetchMock } from '@react-mock/fetch';
 
